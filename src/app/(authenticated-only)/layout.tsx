8927f4a3f6f383dc77d896517c775f37720048ee
HaydenDevK <hayden.dev.k@gmail.com>
2025-10-31 12:40:43 +0900

    Feat: protect paths by auth

---
+++ src/app/(authenticated-only)/layout.tsx
@@ -0,0 +1,21 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function AuthenticatedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  // 경로 보호 : 로그인 안되어 있으면 로그인 페이지로 리다이렉트
+  if (!user || !userError) {
+    redirect("/auth/login");
+  }
+
+  return <>{children}</>;
+}
---
+++ src/app/(non-authenticated-only)/layout.tsx
@@ -0,0 +1,20 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // 경로 보호 : 로그인 안되어 있으면 로그인 페이지로 리다이렉트
+  if (user) {
+    redirect("/");
+  }
+
+  return <>{children}</>;
+}
