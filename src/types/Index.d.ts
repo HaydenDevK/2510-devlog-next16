b6ba8c777b8a56f305d7ded2dc74aa2cd3f370ec
HaydenDevK <hayden.dev.k@gmail.com>
2025-11-28 11:00:44 +0900

    Feat: detail page

--- src/types/Index.d.ts
+++ src/types/Index.d.ts
@@ -1,5 +1,20 @@
-type Profile = Database["public"]["Tables"]["profiles"]["Row"];
-type FormState = {
+import { Database } from "./supabase";
+
+export type Profile = Database["public"]["Tables"]["profiles"]["Row"] | null;
+export type Post = Database["public"]["Tables"]["posts"]["Row"];
+export type Comment = Database["public"]["Tables"]["comments"]["Row"];
+
+export type MainDetail = Post & {
+  profiles: Profile;
+};
+
+export type PostDetail = Post & {
+  profiles: Profile;
+} & {
+  comments: Comment[];
+};
+
+export type FormState = {
   success: boolean;
   error: string | null;
 };
